fe8046af2866af0b8318a79e05253c48e1e20546
OsmondLin1 <117941591+OsmondLin1@users.noreply.github.com>
2022-12-16 15:55:57 -0500

    Added movement

--- Gunman.java
+++ Gunman.java
@@ -10,6 +10,9 @@ public class Gunman extends Actor
 {
     GreenfootImage [] photos = new GreenfootImage[5];
     GreenfootImage [] photosleft = new GreenfootImage[5];
+    SimpleTimer animTimer = new SimpleTimer();
+    String facing = "right";
+    
     public Gunman(){
         for(int i = 0; i < photos.length; i++){
             photos[i] = new GreenfootImage("images/player1_idle/tile00" + i + "-transformed.png");
@@ -26,6 +29,38 @@ public class Gunman extends Actor
     }
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.isKeyDown("")){
+            move(10);
+            facing = "right";
+        }
+        if(Greenfoot.isKeyDown("a")){
+            move(-10);
+            facing = "left";
+        }
+        if(Greenfoot.isKeyDown("w")){
+            setLocation(getX(), getY() - 10);
+        }
+        if(Greenfoot.isKeyDown("s")){
+            setLocation(getX(), getY() + 10);
+        }
+        
+        animate();
+    }
+    
+    int i = 0;
+    public void animate(){
+        
+        if(animTimer.millisElapsed() > 100){
+            if(facing.equals("right")){
+                setImage(photos[i]);
+                i = (i + 1) % photos.length;
+            }
+            else
+            {
+                setImage(photosleft[i]);
+                i = (i + 1) % photosleft.length;
+            }
+            animTimer.mark();
+        }
     }
 }
--- Tryhard.java
+++ Tryhard.java
@@ -10,15 +10,16 @@ public class Tryhard extends Actor
 {
     GreenfootImage [] images = new GreenfootImage[4];
     GreenfootImage [] imagesleft = new GreenfootImage[4];
-    
+    SimpleTimer animTimer = new SimpleTimer();
+    String facing = "right";
     public Tryhard(){
         for(int i = 0; i < images.length; i++){
-            images[i] = new GreenfootImage("images/player2_idle/tile00" + i + "-f7yw8xJvF-transformed.png");
+            images[i] = new GreenfootImage("images/player2_idle/tile00" + i + ".png");
             images[i].scale(100, 100);
         } 
         
         for(int i = 0; i < imagesleft.length; i++){
-            imagesleft[i] = new GreenfootImage("images/player2_idle/tile00" + i + "-f7yw8xJvF-transformed.png");
+            imagesleft[i] = new GreenfootImage("images/player2_idle/tile00" + i + ".png");
             imagesleft[i].mirrorHorizontally();
             imagesleft[i].scale(100, 100);
         }
@@ -26,6 +27,40 @@ public class Tryhard extends Actor
     }
     public void act()
     {
+        if(Greenfoot.isKeyDown("d")){
+            move(10);
+            facing = "right";
+        }
+        if(Greenfoot.isKeyDown("a")){
+            move(-10);
+            facing = "left";
+        }
+        if(Greenfoot.isKeyDown("w")){
+            setLocation(getX(), getY() - 10);
+        }
+        if(Greenfoot.isKeyDown("s")){
+            setLocation(getX(), getY() + 10);
+        }
+        
+        animate();
+    }
+    
+    int i = 0;
+    public void animate(){
+        
+        if(animTimer.millisElapsed() > 100){
+            if(facing.equals("right")){
+                setImage(images[i]);
+                i = (i + 1) % images.length;
+            }
+            else
+            {
+                setImage(imagesleft[i]);
+                i = (i + 1) % imagesleft.length;
+            }
+            animTimer.mark();
+        }
+        
         
     }
 }
